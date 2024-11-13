60fe7176178fd32ceda92e076de8b140b74e35d7
Vaibhav Sinha <39880936+vaibhavsinhacs@users.noreply.github.com>
2024-11-13 11:41:42 -0600

    Add files via upload

---
+++ schema/user.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+/**
+ * Define the Mongoose Schema for a User.
+ */
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  location: String,
+  description: String,
+  occupation: String,
+  login_name: { type: String, unique: true, required: true },
+});
+
+/**
+ * Create a Mongoose Model for a User using the userSchema.
+ */
+const User = mongoose.model("User", userSchema);
+
+/**
+ * Make this available to our application.
+ */
+module.exports = User;
